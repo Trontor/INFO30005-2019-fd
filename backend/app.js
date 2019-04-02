98017cdda475ef18d867ed97264b791936cc80ae
Rohyl <rohyljoshi@hotmail.com>
2019-04-02 17:23:23 +1100

    Added nodemon as dev dependency + more packages

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var app = express();
+
 const PORT = process.env.port || 3000;
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-	res.send('hello world');
-});
+app.get('/', (req, res) => res.send("Hello"));
 
-app.listen(PORT, () => console.log(`Freedom Dive's Hello Food API is listening on port ${port}!`));
+app.listen(PORT, () => console.log(`Freedom Dive's Hello Food API is listening on port ${PORT}!`));

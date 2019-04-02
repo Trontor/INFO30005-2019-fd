13e231f8b2ef5c7e7852dd35139de31ff82e8f4f
Rohyl <rohyljoshi@hotmail.com>
2019-04-02 17:49:41 +1100

    Added basic routing

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var app = express();
+const express = require("express");
+const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
 
+const app = express();
+const db = require("./config/keys").mongoURI;
+
+// If deployed, use assigned port or default to 3000
 const PORT = process.env.port || 3000;
+// Connect to mongoose
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected!"))
+  .catch(err => console.log(err));
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => res.send("Hello"));
+app.get("/", (req, res) => res.send("Hello"));
 
-app.listen(PORT, () => console.log(`Freedom Dive's Hello Food API is listening on port ${PORT}!`));
+// Link routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+
+app.listen(PORT, () =>
+  console.log(`Freedom Dive Hello Food API is listening on port ${PORT}!`)
+);

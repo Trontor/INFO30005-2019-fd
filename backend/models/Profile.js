54224a803eb403c6f173d8694d9ef81bac409fc1
Rohyl <rohyljoshi@hotmail.com>
2019-04-04 11:27:17 +1100

    Began constructing profile schema

---
+++ backend/models/Profile.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  modules: {
+    type: Array
+  }
+});

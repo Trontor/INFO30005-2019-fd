caca8ccc5be4201ca2d6d2cec10fbb380af07c10
Rohyl <rohyljoshi@hotmail.com>
2019-04-05 23:54:28 +1100

    Prepared for Heroku deployment

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://rohyl:rohyl@cluster0-5xdeg.mongodb.net/test?retryWrites=true",
-  secretOrKey: "32ioh4"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}

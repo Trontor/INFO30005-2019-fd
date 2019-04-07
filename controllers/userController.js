02b4de967580815771b98c75a39d7b90738a80f0
Rohyl <rohyljoshi@hotmail.com>
2019-04-07 15:19:58 +1000

    Fixed broken import
    Sorry!

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require("../../models/User");
+const User = require("../models/User");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");

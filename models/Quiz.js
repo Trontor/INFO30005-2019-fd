2d8e9eeaf79e3dc00fcfc3edca3ddc9d4bb74fcb
Rohyl <rohyljoshi@hotmail.com>
2019-04-22 10:06:26 +1000

    Re-structured front-end

--- models/Quiz.js
+++ models/Quiz.js
@@ -1,34 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const QuizSchema = new mongoose.Schema( {
-    content: {
-        type: String,
-        required: true
-    },
-    optionA: {
-        type: String,
-        required: true
-    },
-    optionB: {
-        type: String,
-        required: true
-    },
-    optionC: {
-        type: String,
-        required: true
-    },
-    optionD: {
-        type: String,
-        required: true
-    },
-    answer: {
-        type: String,
-        required: true
-    },
-    starAward: {
-        type: Number,
-        required: true
-    }
+const QuizSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  optionA: {
+    type: String,
+    required: true
+  },
+  optionB: {
+    type: String,
+    required: true
+  },
+  optionC: {
+    type: String,
+    required: true
+  },
+  optionD: {
+    type: String,
+    required: true
+  },
+  answer: {
+    type: String,
+    required: true
+  },
+  starAward: {
+    type: Number,
+    required: true
+  }
 });
 
-module.exports = Quiz = mongoose.model('quiz', QuizSchema);
+module.exports = Quiz = mongoose.model("quiz", QuizSchema);

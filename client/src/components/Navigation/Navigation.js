29de3c086a726a69af5b2791e9a9003bfcff0fbe
Rohyl <rohyljoshi@hotmail.com>
2019-04-28 08:51:51 +1000

    added home link universally

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -59,13 +59,13 @@ class Navigation extends Component {
           </button>
           <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav text-uppercase ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#homeTop">
+                  Home
+                </a>
+              </li>
               {this.props.location.pathname === "/" ? (
                 <>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#homeTop">
-                      Home
-                    </a>
-                  </li>
                   <li class="nav-item">
                     <a class="nav-link" href="#about">
                       About

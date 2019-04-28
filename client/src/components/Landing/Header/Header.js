58ea25ba082e95c3e18d0e988e7586b075228470
Rohyl <rohyljoshi@hotmail.com>
2019-04-28 18:38:12 +1000

    Changed header BG

--- client/src/components/Landing/Header/Header.js
+++ client/src/components/Landing/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
         <div className="h-100 text-center align-items-center">
           <div className="w-100 text-white">
             <div className="intro-heading">
-              Reinvent your school's health and nutrition education
+              Make Health Education Great Again
             </div>
             <div className="intro-text mb-0">
               A modern, curriculum-aligned, health and nutrition education

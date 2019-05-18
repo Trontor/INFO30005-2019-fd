66e5efae961203f36ae60a7be2e470f875a4f7fc
Rohyl <rohyljoshi@hotmail.com>
2019-05-18 15:50:41 +1000

    Added nodemon config + redesigned dashboard header

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -6,39 +6,73 @@ import Loading from "../Loading/Loading";
 import "./Dashboard.scss";
 import DashboardTabs from "./DashboardTabs/DashboardTabs";
 
-const starImg = require("../../images/dashboard/star/star.gif");
+const starImg = require("../../images/dashboard/star.png");
+const todoImg = require("../../images/dashboard/to-do-list.png");
+const checkedImg = require("../../images/dashboard/checked.png");
 
 class Dashboard extends Component {
   componentDidMount() {
     this.props.getCurrentProfile();
   }
+
   render() {
     if (this.props.profile.loading || !this.props.profile.data) {
       return <Loading />;
     }
     const profile = this.props.profile.data;
-    console.log("completed: " + profile.completed.length);
-    // console.log(profile);
+    const completedCount = profile.completed.length;
+    const itemsToComplete =
+      profile.topics
+        .map(topic => topic.items.length)
+        .reduce((acc, val) => acc + val) - completedCount;
     return (
       <>
         <section id="topInterface">
           <div className="container">
             <div className="row">
-              <div className="col-xs-12 col-sm-6 col-md-6">
-                <div className="well well-sm">
-                  <div className="row">
-                    <div className="col-sm-6 col-md-4 avatar text-center">
-                      <img src={profile.avatar} alt="Avatar" />
+              <div className="col-12 col-md-10 offset-md-1">
+                <div className="row">
+                  <div className="col-12 col-lg-2 p-0 center">
+                    <img
+                      className="avatar w-md-100"
+                      src={profile.avatar}
+                      alt="Avatar"
+                    />
+                  </div>
+                  <div className="col-12 col-lg-2 center">
+                    <div className="d-inline-block">
+                      <div id="name">{profile.name}</div>
+                      <div id="email">{profile.email}</div>
                     </div>
-                    <div className="col-sm-6 col-md-8">
-                      <h4>{profile.name}</h4>
-                      <small>{profile.school}</small>
-                      <p>{profile.email}</p>
-                      <p className="d-inline"> Lv.1</p>
-                      <p className="d-inline">
-                        <img alt="Stars" src={starImg} />
-                        x2
-                      </p>
+                  </div>
+                  <div className="col-12 col-lg-4 text-center center">
+                    <div className="d-inline-block text-left">
+                      <div id="teacher">
+                        Teacher:{" "}
+                        <span>
+                          {profile.teacherHonor} {profile.teacherName}
+                        </span>
+                      </div>
+                      <div id="school">
+                        School:
+                        <span>{"   " + profile.school}</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-12 col-lg-4 text-center">
+                    <div className="text-right d-inline-block">
+                      <div id="completed-info">
+                        <span>Completed {completedCount} items</span>
+                        <img id="star-img" src={checkedImg} alt="Avatar" />
+                      </div>
+                      <div id="not-completed">
+                        <span>Need to complete {itemsToComplete} items</span>
+                        <img src={todoImg} alt="Avatar" />
+                      </div>
+                      <div id="completed-stars">
+                        <span>{profile.stars}</span>
+                        <img src={starImg} alt="Avatar" />
+                      </div>
                     </div>
                   </div>
                 </div>
